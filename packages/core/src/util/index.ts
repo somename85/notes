747c3b1fa6728634b97de0ebcdb7ef62930967bf
AlekseiDryndin <aleksei@hindsiteinc.com>
2024-09-24 19:58:16 +0400

    Enabling CORS

--- packages/core/src/util/index.ts
+++ packages/core/src/util/index.ts
@@ -24,6 +24,10 @@ export module Util {
             return {
                 body,
                 statusCode,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": true,
+                },
             };
         };
     }
